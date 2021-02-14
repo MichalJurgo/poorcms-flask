1796768e053a6fe204269c06a5143371f3ef80b0
vonMajkel <mpjurgo@gmail.com>
2021-02-14 13:32:03 +0100

    Add static page view

--- poorcms/routes.py
+++ poorcms/routes.py
@@ -9,6 +9,12 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/page/<int:page_id>')
+def static_page(page_id):
+    page = StaticPage.query.get_or_404(page_id)
+    return render_template('static_page.html', page=page)
+
+
 @app.context_processor
 def inject_menu_pages():
     menu_pages = StaticPage.query.filter_by(in_menu=True).all()

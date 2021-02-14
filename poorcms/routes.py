c85018b88b021bd52e9f8cdaad186b9144131274
vonMajkel <mpjurgo@gmail.com>
2021-02-14 12:55:13 +0100

    Create menu pages context processor and auto add pages to menu

--- poorcms/routes.py
+++ poorcms/routes.py
@@ -7,3 +7,9 @@ from poorcms.models import StaticPage
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.context_processor
+def inject_menu_pages():
+    menu_pages = StaticPage.query.filter_by(in_menu=True).all()
+    return dict(menu_pages=menu_pages)

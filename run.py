dc1c4a7df413dc434b02ddac8d8896d5aae4ffe9
Michal <mpjurgo@gmail.com>
2021-02-28 16:15:57 +0100

    Add flask-migrate

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
+from flask_migrate import Migrate, upgrade
+
 from poorcms import app, db
 from poorcms.models import User, StaticPage
+
+migrate = Migrate(app, db)
+
 
 @app.shell_context_processor
 def make_shell_context():
